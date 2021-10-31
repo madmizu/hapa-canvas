0b7c8f8bb9e1fd2b3feb70b0f68bb3d795d63fbc
Madison Shimizu <mhshimizu@outlook.com>
2021-10-30 19:50:56 -1000

    added brackets to shrink scroll

--- script.js
+++ script.js
@@ -3,10 +3,11 @@ window.onscroll = function() {scrollFunction()}
 
 function scrollFunction() {
         if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
-            document.getElementsByClassName("navbar").style.padding = "30px 10px";
+            document.getElementsByClassName("navbar")[0].style.padding = "30px 10px";
             document.getElementById("logo").style.fontSize = "25px";
         } else {
-            document.getElementsByClassName("navbar").style.padding = "80px 10px";
-            document.getElementById("logo").style.fontSize = "35px";
+            document.getElementsByClassName("navbar")[0].style.padding = "80px 10px";
+            document.getElementById("logo") .style.fontSize = "35px";
         }
      }
+
