d9a4c1af647703fb9526b8aac81cd1de81ecde4f
Madison Shimizu <mhshimizu@outlook.com>
2021-10-30 22:39:14 -1000

    fix shrink scroll

--- script.js
+++ script.js
@@ -3,11 +3,11 @@ window.onscroll = function() {scrollFunction()}
 
 function scrollFunction() {
         if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
-            document.getElementsByClassName("navbar")[0].style.padding = "10px, 10px, 45px";
+            document.getElementsByClassName("navbar")[0].style.padding = "30px 10px 20px 10px";
             document.getElementById("logo").style.fontSize = "25px";
         } else {
             document.getElementsByClassName("navbar")[0].style.padding = "90px 10px";
-            document.getElementById("logo") .style.fontSize = "35px";
+            document.getElementById("logo") .style.fontSize = "50px";
         }
      }
 
