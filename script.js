60d2d985f9c12a1939668e3ad65f34bf297bc751
Madison Shimizu <mhshimizu@outlook.com>
2021-10-30 22:11:25 -1000

    increased smallest padding for shrink scroll

--- script.js
+++ script.js
@@ -3,10 +3,10 @@ window.onscroll = function() {scrollFunction()}
 
 function scrollFunction() {
         if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
-            document.getElementsByClassName("navbar")[0].style.padding = "30px 10px";
+            document.getElementsByClassName("navbar")[0].style.padding = "40px 10px";
             document.getElementById("logo").style.fontSize = "25px";
         } else {
-            document.getElementsByClassName("navbar")[0].style.padding = "80px 10px";
+            document.getElementsByClassName("navbar")[0].style.padding = "90px 10px";
             document.getElementById("logo") .style.fontSize = "35px";
         }
      }
