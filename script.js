abd026d245cc8b66faec1a4804a204cf33962720
Madison Shimizu <mhshimizu@outlook.com>
2021-10-30 17:26:50 -1000

    get elements by class 'name'

--- script.js
+++ script.js
@@ -3,10 +3,10 @@ window.onscroll = function() {scrollFunction()}
 
 function scrollFunction() {
         if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
-            document.getElementByClass("navbar").style.padding = "30px 10px";
+            document.getElementsByClassName("navbar").style.padding = "30px 10px";
             document.getElementById("logo").style.fontSize = "25px";
         } else {
-            document.getElementByClass("navbar").style.padding = "80px 10px";
+            document.getElementsByClassName("navbar").style.padding = "80px 10px";
             document.getElementById("logo").style.fontSize = "35px";
         }
      }
