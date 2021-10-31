12e99c52f282225c265e89c9c4b90c6892861696
Madison Shimizu <mhshimizu@outlook.com>
2021-10-30 22:49:03 -1000

    decreased top padding

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ window.onscroll = function() {scrollFunction()}
 
 function scrollFunction() {
         if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
-            document.getElementsByClassName("navbar")[0].style.padding = "30px 10px 20px 10px";
+            document.getElementsByClassName("navbar")[0].style.padding = "20px 10px 40px 10px";
             document.getElementById("logo").style.fontSize = "25px";
         } else {
             document.getElementsByClassName("navbar")[0].style.padding = "90px 10px";
